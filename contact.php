079ecf22b622cd840fe050bb3e9e67600bad9650
tayebhakkar <tayebhakkar@yahoo.fr>
2022-01-18 18:12:35 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,6 +8,6 @@ if(isset($_POST)){
     $mailTo = 'tayebhakkar@yahoo.fr';
     $head = "From: ".$email;
     $text = "Mail reçu de la part de : ".$name.".\n\n".$message;
-
+    mail($mailTo,$subject,$text,$head);
 }
 ?>
