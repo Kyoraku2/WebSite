7bb6b5be07857fb0fe84a4f0aee58ef2f8612225
tayebhakkar <tayebhakkar@yahoo.fr>
2022-01-18 18:11:14 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,6 @@ if(isset($_POST)){
     $mailTo = 'tayebhakkar@yahoo.fr';
     $head = "From: ".$email;
     $text = "Mail reçu de la part de : ".$name.".\n\n".$message;
-    mail($mailTo,$subject,$text,$head);
-    header("Location: ".$_SERVER['HTTP_REFERER']);
+
 }
 ?>
