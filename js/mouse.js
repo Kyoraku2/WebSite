179cced95ed936bd16162f7583904d0f56476d4d
thakkar <tayebhakkar@yahoo.fr>
2023-09-16 19:24:24 +0200

    Typos

--- js/mouse.js
+++ js/mouse.js
@@ -1,12 +1,6 @@
 document.body.addEventListener("mousemove", evt => {
     const mouseX = evt.clientX;
     const mouseY = evt.clientY;
-    
-    /*gsap.set(".cursor", {
-      x: mouseX,
-      y: mouseY
-    })*/
-    
     gsap.to(".shape", {
       x: mouseX,
       y: mouseY,
